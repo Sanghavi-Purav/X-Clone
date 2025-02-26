216315f5e8dbbf7a06bf60d5839cfe0080eafee4
Sanghavi-Purav <sanghavipurav5@gmail.com>
2025-02-26 11:34:47 +0530

    commit changes

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -39,6 +39,11 @@ export const followUnfollowUser = async (req, res) => {
 		if (isFollowing) {
 			await User.findByIdAndUpdate(id, { $pull: { followers: req.user._id } });
 			await User.findByIdAndUpdate(req.user._id, { $pull: { following: id } });
+			notification = new Notification({
+				from: req.user._id,
+				to: id,
+				type: "follow",
+			});
 
 			res.status(200).json({ message: "User unfollowed successfully" });
 		} else {
