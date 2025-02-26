3f50b5b81bf0c6a27fba2ecf644ea32535985602
Sanghavi-Purav <sanghavipurav5@gmail.com>
2025-02-26 09:59:49 +0530

    final changes made

--- backend/controllers/notificationControllers.js
+++ backend/controllers/notificationControllers.js
@@ -8,12 +8,18 @@ export const getNotifications = async (req, res) => {
 			.populate({ path: "from", select: "username profileImg" });
 		await Notification.updateMany({ to: userId }, { read: true });
 		res.status(200).json(all_notifications);
-	} catch (error) {}
+	} catch (error) {
+		console.log(`Error in the getNotifications Controller ${error}`);
+		res.status(500).json({ message: "Internal Server Error" });
+	}
 };
 export const deleteNotifications = async (req, res) => {
 	try {
 		const userId = req.user._id;
 		await Notification.deleteMany({ to: userId });
 		res.status(200).json({ message: "All notifications are deleted" });
-	} catch (error) {}
+	} catch (error) {
+		console.log(`Error in the deleteNotifications Controller ${error}`);
+		res.status(500).json({ message: "Internal Server Error" });
+	}
 };
