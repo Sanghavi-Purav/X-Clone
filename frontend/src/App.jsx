146af5497f40ae56a503cecaa7a90d78d7e32f0c
Sanghavi-Purav <sanghavipurav5@gmail.com>
2025-03-01 17:48:11 +0530

    my second frontend commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,9 @@ import SignUpPage from "./pages/signup/SignUpPage";
 import LoginPage from "./pages/login/LoginPage";
 import Sidebar from "./components/common/Sidebar.jsx";
 import RightPanel from "./components/common/RightPanel.jsx";
+import NotificationPage from "./pages/notification/NotificationPage.jsx";
+import ProfilePage from "./pages/profile/ProfilePage.jsx";
+import { Toaster } from "react-hot-toast";
 
 function App() {
 	return (
@@ -13,8 +16,11 @@ function App() {
 				<Route path="/" element={<HomePage />} />
 				<Route path="/signup" element={<SignUpPage />} />
 				<Route path="/login" element={<LoginPage />} />
+				<Route path="/notifications" element={<NotificationPage />} />
+				<Route path="/profile/:username" element={<ProfilePage />} />
 			</Routes>
 			<RightPanel />
+			<Toaster />
 		</div>
 	);
 }
